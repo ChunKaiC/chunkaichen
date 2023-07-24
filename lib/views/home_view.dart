e1e0a8607e1dd3e4c7cef66f514803b385f42b14
ChunKaiC <baj49jab@gmail.com>
2023-07-23 23:47:59 -0400

    init structure & home view

---
+++ lib/views/home_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.blue,
+              Colors.white,
+            ],
+          ),
+        ),
+        child: Center(child: Text('Under Construction!')),
+      ),
+    );
+  }
+}
